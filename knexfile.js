13cf4c234a7f3099a1b1e886dc30f29c8e3d866b
alexisjcarr <acarr59@gmail.com>
2019-07-15 13:03:13 -0500

    ✅ Finishes guided project
     #1

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,11 @@
+// Update with your config settings.
+
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './data/posts.db3',
+      filename: "./data/posts.db3"
     },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './data/migrations',
-    },
-    seeds: {
-      directory: './data/seeds',
-    },
-  },
+    useNullAsDefault: true
+  }
 };
--- posts/post-router.js
+++ posts/post-router.js
@@ -1,28 +1,57 @@
-const express = require('express');
+const express = require("express");
 
 // database access using knex
-const db = require('../data/db-config.js');
+const db = require("../data/db-config.js");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+router.get("/", async (req, res) => {
+  try {
+    const posts = await db("posts");
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {});
 
+router.post("/", async (req, res) => {
+  const post = req.body;
+
+  try {
+    const insPost = await db("posts").insert(post, "id");
+    res.status(201).json(insPost[0]);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
 });
 
-router.post('/', (req, res) => {
-
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const count = await db("posts").where({ id: id });
+    if (count > 0) {
+      res.status(200).json({ message: `${count} record(s) updated.` });
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
 
-});
-
-router.delete('/:id', (req, res) => {
-
+  try {
+    const count = await db("posts")
+      .where({ id: id })
+      .del();
+    res.status(200).json({ message: `${count} record(s) deleted.` });
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
 });
 
 module.exports = router;
